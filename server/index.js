18e3e747a7c3ae5532a6fee8e2222430b4529ffb
Your Name <you@example.com>
2021-01-24 23:51:37 +0300

    delete function close connect to Data Base

--- server/index.js
+++ server/index.js
@@ -2,14 +2,40 @@ const express = require('express');
 const app = express();
 const mysql = require("mysql");
 
+
+//Запуск сервера на порту 3001?
 app.listen(3001,()=>{
     console.log("running on port 3001");
 });
 
-app.get("/",(request,response)=>{
-    response.send("HELLO");
+//конфигурация базы данных
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "testcrudapp"
 });
 
-const db = mysql.createConnection({
 
+// тестирование подключения к базе данных
+db.connect((err)=>{
+   if(err){
+       return console.error("Error: " + err.message);
+   }
+   else {
+       console.log("Connection to DataBase is successful!");
+   }
+});
+
+
+//Выполнение ответов сервера
+app.get("/",(request,response)=>{
+
+    const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES ('Harry Potter','Good movie')";
+    db.query(sqlInsert,(err,result)=>{
+        console.log(err);
+        console.log(result);
+
+        response.send("HELLO");
+    });
 });
